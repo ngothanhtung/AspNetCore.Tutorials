fe6d6a88dfe4ddc926a582d54dd7dcd348cf7bbe
Ngo Thanh Tung <tung.ngo@outlook.com>
2017-06-14 23:47:58 +0700

    init project

---
+++ AspNetCore.Tutorials/ViewComponents/CitySummary.cs
@@ -0,0 +1,31 @@
+﻿﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore.Tutorials
+{
+	//Cách 1: public class CitySummary : ViewComponent
+	//Cách 2: public class CitySummaryViewComponent : ViewComponent
+	//Cách 3: Dùng attribute [ViewComponent(Name = "CitySummary")]
+	public class CitySummaryViewComponent : ViewComponent
+    {
+        public CitySummaryViewComponent()
+        {
+        }
+
+        /*
+        public string Invoke() 
+        {
+            return $"Danang City";
+        }
+        */
+
+        public async Task<IViewComponentResult> InvokeAsync() 
+        {
+            var people = new[] {
+                "Peter", "John"
+            };
+            return View(people);
+        }
+    }
+}
